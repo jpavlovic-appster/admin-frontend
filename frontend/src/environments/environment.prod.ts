372ff4a599a9ce34c20a163df4253c1ab940b82b
jpavlovic-appster <121563356+jpavlovic-appster@users.noreply.github.com>
2024-08-16 12:50:10 +0200

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
     production: false,
     API_URL: 'https://stag-adminapi.codezania.com/api/',
     STATIC_URL: 'https://stag-adminapi.codezania.com/',
-    SUPER_URL: 'https://admin.vecihi.fun',
-    SUB_URL: 'https://subscriber.vecihi.fun',
+    SUPER_URL: 'admin.vecihi.fun',
+    SUB_URL: 'subscriber.vecihi.fun',
     DEV_URL: '',
     DOCUMENTATION_FILE: "documentation.json"
   };
