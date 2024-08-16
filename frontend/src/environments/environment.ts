7ee372b220864551f857ada8b957f16fa0608ff0
jpavlovic-appster <121563356+jpavlovic-appster@users.noreply.github.com>
2024-08-16 12:35:00 +0200

    Create environment.ts

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    API_URL: 'https://stag-adminapi.codezania.com/api/',
+    STATIC_URL: 'https://stag-adminapi.codezania.com/',
+    SUPER_URL: 'https://admin.vecihi.fun',
+    SUB_URL: 'https://subscriber.vecihi.fun',
+    DEV_URL: '',
+    DOCUMENTATION_FILE: "documentation.json"
+  };
