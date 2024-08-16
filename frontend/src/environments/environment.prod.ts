47100ed50a116fc189c673f82b3e768d4edf12d4
jpavlovic-appster <121563356+jpavlovic-appster@users.noreply.github.com>
2024-08-16 13:23:53 +0200

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    API_URL: 'https://admin-api.vecihi.fun//api/',
+    API_URL: 'https://admin-api.vecihi.fun/api/',
     STATIC_URL: 'https://admin-api.vecihi.fun/',
     SUPER_URL: 'admin.vecihi.fun',
     SUB_URL: 'subscriber.vecihi.fun',
