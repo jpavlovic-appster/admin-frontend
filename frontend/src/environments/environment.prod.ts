7a69433ea8e72741e1d76b462a9eab4e8d0b24d2
jpavlovic-appster <121563356+jpavlovic-appster@users.noreply.github.com>
2024-08-16 18:43:56 +0200

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
-    production: false,
-    API_URL: 'https://admin-api.vecihi.fun/api/',
-    STATIC_URL: 'https://admin-api.vecihi.fun/',
-    SUPER_URL: 'admin.vecihi.fun',
-    SUB_URL: 'subscriber.vecihi.fun',
-    DEV_URL: '',
-    DOCUMENTATION_FILE: "documentation.json"
+  production: false,
+  API_URL: 'https://admin-api.acegaming.tech/api/',
+  STATIC_URL: 'https://admin-api.acegaming.tech/',
+  SUPER_URL: 'admin.acegaming.tech',
+  SUB_URL: 'subscriber.acegaming.tech',
+  DEV_URL: '',
+  DOCUMENTATION_FILE: "documentation.json"
   };
